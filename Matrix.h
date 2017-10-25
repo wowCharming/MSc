7c76527b6ccbe2574b350d9bb2009c4fa5a28632
wowCharming <xuminchen@outlook.com>
2017-10-25 10:34:47 +0200

    Add files via upload

---
+++ Matrix.h
@@ -0,0 +1,272 @@
+#pragma once
+#include <iostream>
+#include <fstream>  
+#include <sstream>  
+#include <vector>  
+#include <cassert>
+#include <algorithm>
+#include <assert.h>
+
+using std::vector;
+using std::string;
+using std::cout;
+using std::cin;
+using std::istream;
+using std::ostream;
+using std::endl;
+
+
+class Matrix
+{
+public:
+	Matrix(int r, int c) : row(r), col(c)
+	{
+		int size = row * col;
+		assert(size >= 0);
+		data_ = new double[size];
+	}
+
+
+	Matrix(int r, int c, double initialValue) : row(r), col(c)
+	{
+		int size = row * col;
+		assert(size >= 0);
+		data_ = new double[size];
+
+		for (int i = 0; i < row; i++)
+		{
+			for (int j = 0; j < col; j++)
+			{
+				data_[i * col + j] = initialValue;
+			}
+		}
+	}
+
+	Matrix(const Matrix & m)
+	{
+		int size = m.row * m.col;
+		data_ = new double[size];
+		std::copy(m.data_, m.data_ + size, data_);
+
+		//do not work
+		/*for (int i = 0; i < m.row; i++)
+		{
+			for (int j = 0; j < m.col; j++)
+			{
+				data_[i * col + j] = m.data_[i * col + j];
+			}
+		}*/
+	}
+
+
+	~Matrix()
+	{
+		delete [] data_;
+	};
+
+	int getColumns() const
+	{
+		return col;
+	}
+
+	int getRows() const
+	{
+		return row;
+	}
+
+	double operator() (int i, int j) const
+	{
+		assert(i >= 0 && i < row && j >= 0 && j < col);
+		return data_[i * col + j];
+	}
+
+	double & operator() (int i, int j)
+	{
+		assert(i >= 0 && i < row && j >= 0 && j < col);
+		return data_[i * col + j];
+	}
+
+
+	Matrix & operator= (const Matrix & m);
+	Matrix operator+(const Matrix & m);
+	Matrix operator-(const Matrix & m);
+	Matrix operator*(const Matrix & m);
+	Matrix operator+=(const Matrix & m);
+	Matrix operator-=(const Matrix & m);
+	Matrix operator*=(const Matrix & m);
+	bool operator==(const Matrix & m);
+	bool operator!=(const Matrix & m);
+
+private:
+	int row;
+	int col;
+	//int currentRow;
+
+	double * data_;
+
+};
+
+Matrix &Matrix::operator= (const Matrix & m)
+{
+	for (int i = 0; i < row; i++)
+	{
+		for (int j = 0; j < col; j++)
+		{
+			data_[i * col + j] = m.data_[i * col + j];
+		}
+	}
+	return *this;
+}
+
+
+Matrix Matrix::operator+ (const Matrix & m)
+{
+	Matrix m0(row, col);//if ini val
+	assert(this->row == m.row && col == m.col);
+
+	for (int i = 0; i < row; i++)
+	{
+		for (int j = 0; j < col; j++)
+		{
+			m0.data_[i * col + j] = data_[i * col + j] + m.data_[i * col + j];//left use m0
+		}
+	}
+
+	return m0;
+};
+
+Matrix Matrix::operator- (const Matrix & m)
+{
+	Matrix m0(row, col);
+	assert(this->row == m.row && col == m.col);
+
+	for (int i = 0; i < row; i++)
+	{
+		for (int j = 0; j < col; j++)
+		{
+			m0.data_[i * col + j] = data_[i * col + j] - m.data_[i * col + j];//left use m0
+		}
+	}
+
+	return m0;
+};
+
+Matrix Matrix::operator* (const Matrix & m)
+{
+	assert(this->col == m.row);
+	Matrix m0(row, m.col);
+
+	for (int i = 0; i < row; ++i)
+	{
+		for (int j = 0; j < m.col; ++j)
+		{
+			double sum = 0.0;
+			for (int p = 0; p < col; ++p)
+			{
+				sum += data_[i * col + p] * m.data_[p * m.col + j];//problem here: m.col
+			}
+			//m0.data_[i * m.col + j] = sum; //correction:i * m.col + j
+			m0(i, j) = sum;//correction here!!!!
+		}
+	}
+
+	return m0;
+};
+
+Matrix Matrix::operator+= (const Matrix & m)
+{
+	assert(this->row == m.row && col == m.col);
+
+	for (int i = 0; i < row; i++)
+	{
+		for (int j = 0; j < col; j++)
+		{
+			data_[i*col + j] = m.data_[i*col + j] + data_[i*col + j];
+		}
+	}
+
+	return  *this;
+}
+
+Matrix Matrix::operator-= (const Matrix & m)
+{
+	assert(this->row == m.row && col == m.col);
+
+	for (int i = 0; i < row; i++)
+	{
+		for (int j = 0; j < col; j++)
+		{
+			data_[i*col + j] = m.data_[i*col + j] - data_[i*col + j];
+		}
+	}
+
+	return  *this;
+}
+
+//m*n(i*p) & n*n(p*j)
+Matrix Matrix::operator*= (const Matrix & m)
+{
+	assert(this->col == m.row);
+	Matrix m0(row, m.col);
+
+	for (int i = 0; i < row; i++)
+	{
+		for (int j = 0; j < m.col; j++)
+		{
+			double sum = 0;
+			for (int p = 0; p < col; p++)
+			{
+				sum += data_[i * col + p] * m.data_[p * col + j];
+			}
+			//data_[i * col + j] = sum;
+			m0(i, j) = sum;
+		}
+	}
+	*this = m0;//add here to return the new this
+	return  *this;
+}
+
+bool Matrix::operator== (const Matrix & m)
+{
+	if (this->row == m.row && col == m.col)
+	{
+		for (int i = 0; i < row; i++)
+		{
+			for (int j = 0; j < col; j++)
+			{
+				if (std::abs((i, j) - m(i, j)) > 1e-13)
+				//if (data_[i * col + j] != m.data_[i * col + j])
+				{
+					return false;
+				}
+			}
+		}
+		return true;
+
+	}
+	else
+	{
+		return false;
+	}
+}
+
+bool Matrix::operator!= (const Matrix & m)
+{
+	return !(operator==(m));
+}
+
+std::ostream & operator<<(std::ostream & os, const Matrix & m)
+{
+	int C = m.getColumns();
+	int R = m.getRows();
+	for (int i = 0; i < R; i++)
+	{
+		for (int j = 0; j < C; j++)
+		{
+			os << m(i, j) << " "; //no comma
+		}
+		os << endl;
+	}
+	return os;
+}
+
---
+++ MatrixProduct.cpp
@@ -0,0 +1,72 @@
+#include "Matrix.h"
+#include <iostream>
+#include <cassert>
+#include <cmath>
+
+using std::cout;
+using std::endl;
+
+
+int main(int argc, char** argv)
+{
+	int s1, s2, s3;
+	cout << "please input three integral numbers" << endl;
+	cin >> s1 >> s2 >> s3;
+	if (!s1 * s2 * s3 > 0) cout << "please check the input numbers" << endl;
+	
+	Matrix m1(s1, s2);
+	Matrix m2(s2, s3);
+	Matrix m3(s1, s3);
+	double temp;
+
+	cout << "please input menbers of m1" << endl;
+	for (int i = 0; i < s1; i++)
+	{
+		for (int j = 0; j < s2; j++)
+		{
+			cout << "input m1(" << i << "," << j << ")" << endl;
+			cin >> temp;
+			m1(i, j) = temp;
+		}
+
+	}
+
+	cout << "please input menbers of m2" << endl;
+	for (int i = 0; i < s2; i++)
+	{
+		for (int j = 0; j < s3; j++)
+		{
+			cout << "input m2(" << i << "," << j << ")" << endl;
+			cin >> temp;
+			m2(i, j) = temp;
+		}
+
+	}
+
+	m3 = m1 * m2;
+	
+	cout << "here is m1: " << endl << m1 << endl;
+	cout << "here is m2: " << endl << m2 << endl;
+	cout << "here is the answer m3:" << endl;
+	cout << m3 << endl;
+	getchar();
+	cout << "end" << endl;
+	getchar();
+	/* TEST PART
+	Matrix m1(3, 2, 1);
+	Matrix m2(2, 3, 1);
+	//Matrix m3(1, 2, 1);
+	Matrix m3 = m1 * m2;
+	//Matrix m1 *= m2;
+	
+	cout << m1 << endl;
+	cout << "===========" << endl;
+	cout << m2 << endl;
+	cout << "===========" << endl;
+	cout << m3 << endl;
+	cout << "===========" << endl;
+	m1 *= m2;
+	cout << m1 << endl;
+	cout << "===========" << endl;
+	getchar();*/
+}
